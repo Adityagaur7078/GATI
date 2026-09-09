a39f21c66e08177f90c188d8fd924a1d89565e5f
Adityagaur7078 <adityagaur7078@gmail.com>
2026-09-09 23:04:30 +0530

    feat(frontend): add location search and vehicle selection panels

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
-const LocationSearchPanel = () => {
+const LocationSearchPanel = (props) => {
+
+  const locations = [
+    "Kolkata, West Bengal",
+    "Pune, Maharashtra", "Hyderabad, Telangana",
+    "Amritsar, Punjab", "Lucknow, Uttar Pradesh",
+    "Rishikesh, Uttarakhand",
+    "Shimla, Himachal Pradesh",
+    "Kochi, Kerala"
+  ];
+
   return (
-    <div>LocationSearchPanel</div>
+    <div>
+      {
+        locations.map(function(e, index){
+          return <div key={index} onClick={() => {
+            props.setVehiclePanelOpen(true)
+            props.setPanelOpen(false)
+          }} className="flex gap-4 border-2 p-3 border-gray-50 active:border-black rounded-xl items-center my-2 justify-start">
+        <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-line"></i></h2>
+        <h4 className='font-medium'>{e}</h4>
+      </div>
+        })
+      }
+    </div>
   )
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,14 +5,19 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import "remixicon/fonts/remixicon.css";
 import LocationSearchPanel from "../components/LocationSearchPanel";
+import gaticarimage from "../assets/gaticarimage.png";
+import gatibike from "../assets/gatibike.png";
+import gatiautoimage from "../assets/gatiautoimage.png";
 
 const Home = () => {
   const [pickup, setPickup] = useState("");
   const [destination, setDestination] = useState("");
   const [panelOpen, setPanelOpen] = useState(false);
+  const [vehiclePanelOpen, setVehiclePanelOpen] = useState(false);
 
   const panelRef = useRef(null);
   const panelCloseRef = useRef(null);
+  const vehiclePanelRef = useRef(null);
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -23,7 +28,7 @@ const Home = () => {
       if (panelOpen) {
         gsap.to(panelRef.current, {
           height: "70%",
-          padding: "24px",
+          padding: "0px 24px 24px 24px",
           duration: 0.3,
           ease: "power2.out",
         });
@@ -49,16 +54,27 @@ const Home = () => {
     { dependencies: [panelOpen] }
   );
 
+  useGSAP(() => {
+    if (vehiclePanelOpen){
+      gsap.to(vehiclePanelRef.current,{
+      transform:'translateY(0)'
+    })
+    } else{
+      gsap.to(vehiclePanelRef.current,{
+        transform:'translateY(100%)'
+      })
+    }
+  }, [vehiclePanelOpen])
+
   return (
     <div className="h-screen relative overflow-hidden">
-      {/* Logo */}
+
       <img
         className="w-16 absolute left-5 top-5 z-10"
         src={gatilogoblack}
         alt="Gati"
       />
 
-      {/* Map */}
       <div className="h-screen w-screen">
         <img
           className="h-full w-full object-cover"
@@ -67,76 +83,158 @@ const Home = () => {
         />
       </div>
 
-      {/* Bottom container */}
       <div className="flex flex-col justify-end h-screen absolute top-0 w-full">
-        {/* Search section */}
+
         <div
-  className={`h-[30%] px-6 pb-6 bg-white relative transition-all duration-300 ${
-    panelOpen ? "pt-16" : "pt-6"
-  }`}
->
-  {/* Close button */}
-  <h5
-    ref={panelCloseRef}
-    onClick={() => setPanelOpen(false)}
-    className="opacity-0 absolute right-5 top-16 text-2xl cursor-pointer"
-  >
-    <i className="ri-arrow-down-s-line"></i>
-  </h5>
+          className={`h-[30%] px-6 pb-0.5 bg-white relative transition-all duration-300 ${panelOpen ? "pt-16" : "pt-6"
+            }`}
+        >
 
-  <h4 className="text-2xl font-semibold">
-    Find a trip
-  </h4>
+          <h5
+            ref={panelCloseRef}
+            onClick={() => setPanelOpen(false)}
+            className="opacity-0 absolute right-5 top-20 text-2xl cursor-pointer"
+          >
+            <i className="ri-arrow-down-s-line"></i>
+          </h5>
 
-  <form onSubmit={submitHandler}>
-    {/* Inputs container */}
-    <div className="relative mt-5">
+          <h4 className="text-2xl font-semibold pt-4">
+            Find a trip
+          </h4>
 
-      {/* Vertical line */}
-      <div className="absolute left-4 top-3.5 bottom-3.5 w-0.75 bg-gray-800 rounded-full z-10">
-        
-        {/* Top dot */}
-        <div className="absolute w-1.75 h-1.75 bg-gray-800 rounded-full -left-0.5 -top-px"></div>
+          <form onSubmit={submitHandler}>
 
-        {/* Bottom dot */}
-        <div className="absolute w-1.75 h-1.75 bg-gray-800 rounded-full -left-0.5 -bottom-px"></div>
+            <div className="relative mt-5">
 
-      </div>
+              <div className="absolute left-4 top-3.5 bottom-3.5 w-0.75 bg-gray-800 rounded-full z-10">
 
-      {/* Pickup */}
-      <input
-        onClick={() => setPanelOpen(true)}
-        value={pickup}
-        onChange={(e) => setPickup(e.target.value)}
-        className="bg-[#eee] px-12 py-2 text-base rounded-lg w-full border border-gray-300"
-        type="text"
-        placeholder="Add a pick-up location"
-      />
+                <div className="absolute w-1.75 h-1.75 bg-gray-800 rounded-full -left-0.5 -top-px"></div>
 
-      {/* Destination */}
-      <input
-        onClick={() => setPanelOpen(true)}
-        value={destination}
-        onChange={(e) => setDestination(e.target.value)}
-        className="bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-3 border border-gray-300"
-        type="text"
-        placeholder="Enter your destination"
-      />
+                <div className="absolute w-1.75 h-1.75 bg-gray-800 rounded-full -left-0.5 -bottom-px"></div>
 
-    </div>
-  </form>
-</div>
+              </div>
 
+              <input
+                onClick={() => setPanelOpen(true)}
+                value={pickup}
+                onChange={(e) => setPickup(e.target.value)}
+                className="bg-[#eee] px-12 py-2 text-base rounded-lg w-full border border-gray-300"
+                type="text"
+                placeholder="Add a pick-up location"
+              />
 
-        {/* Location search panel */}
+              <input
+                onClick={() => setPanelOpen(true)}
+                value={destination}
+                onChange={(e) => setDestination(e.target.value)}
+                className="bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-3 border border-gray-300"
+                type="text"
+                placeholder="Enter your destination"
+              />
+
+            </div>
+          </form>
+        </div>
+
         <div
           ref={panelRef}
           className="bg-white h-0 overflow-hidden"
         >
-          <LocationSearchPanel />
+          <LocationSearchPanel setVehiclePanelOpen={setVehiclePanelOpen} setPanelOpen={setPanelOpen} />
         </div>
+
+        <div ref={vehiclePanelRef} className="fixed bottom-0 z-10 w-full bg-white px-3 py-6 translate-y-full">
+          <h3 className="mb-5 text-2xl font-semibold">Choose a Vehicle</h3>
+
+          <div onClick={() => {
+            setVehiclePanelOpen(false)
+          }} className="flex w-full items-center justify-between rounded-xl border-2 mb-2 active:border-black border-gray-300 p-3">
+            <img
+              className="h-12 object-contain"
+              src={gaticarimage}
+              alt="GATI vehicle"
+            />
+
+            <div className="w-1/2 ml-2">
+              <h4 className="flex items-center gap-2 text-base font-medium">
+                GatiGo
+                <span className="flex items-center gap-1">
+                  <i className="ri-user-line"></i>
+                  4
+                </span>
+              </h4>
+
+              <h5 className="text-sm font-medium">2 mins away</h5>
+
+              <p className="text-xs font-normal text-gray-600">
+                Affordable, compact rides
+              </p>
+            </div>
+
+            <h2 className="text-lg font-semibold">₹195.20</h2>
+          </div>
+
+          <div onClick={() => {
+            setVehiclePanelOpen(false)
+          }} className="flex w-full items-center justify-between rounded-xl border-2 mb-2 active:border-black border-gray-300 p-3">
+            <img
+              className="h-12 object-contain"
+              src={gatibike}
+              alt="GATI vehicle"
+            />
+
+            <div className="w-1/2">
+              <h4 className="flex items-center gap-2 text-base font-medium">
+                Moto
+                <span className="flex items-center gap-1">
+                  <i className="ri-user-line"></i>
+                  1
+                </span>
+              </h4>
+
+              <h5 className="text-sm font-medium">3 mins away</h5>
+
+              <p className="text-xs font-normal text-gray-600">
+                Affordable motorcycle rides
+              </p>
+            </div>
+
+            <h2 className="text-lg font-semibold">₹65</h2>
+          </div>
+
+          <div onClick={() => {
+            setVehiclePanelOpen(false)
+          }} className="flex w-full items-center justify-between rounded-xl border-2 mb-2 active:border-black border-gray-300 p-3">
+            <img
+              className="h-12 object-contain"
+              src={gatiautoimage}
+              alt="GATI vehicle"
+            />
+
+            <div className="w-1/2 ml-5">
+              <h4 className="flex items-center gap-2 text-base font-medium">
+                GatiAuto
+                <span className="flex items-center gap-1">
+                  <i className="ri-user-line"></i>
+                  3
+                </span>
+              </h4>
+
+              <h5 className="text-sm font-medium">2 mins away</h5>
+
+              <p className="text-xs font-normal text-gray-600">
+                Affordable auto rides
+              </p>
+            </div>
+
+            <h2 className="text-lg font-semibold">₹118.21</h2>
+          </div>
+
+        </div>
+
+
       </div>
-    </div>
+    </div >
   );
 };
 
